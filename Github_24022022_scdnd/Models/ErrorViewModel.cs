235fd488abf6ecee4428cef043b494fee4b6192b
SudeeshGopi <sudeesh.accrehealthtech@gmail.com>
2022-02-24 17:04:22 +0530

    Add project files.

---
+++ Github_24022022_scdnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Github_24022022_scdnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
